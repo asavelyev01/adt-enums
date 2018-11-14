7aeb0ce8631fd9f88282a9fe822718e1f08e5b35
anton.savelyev <anton.savelyev@vimpelcom.com>
2018-11-14 18:30:30 +0300

    remove explicit type from EnumColumnTypes#enumColumnType

--- case-enum-slick/src/main/scala/com/veon/ep/enums/slick/EnumColumnTypes.scala
+++ case-enum-slick/src/main/scala/com/veon/ep/enums/slick/EnumColumnTypes.scala
@@ -8,7 +8,7 @@ import scala.reflect.ClassTag
 trait EnumColumnTypes {
   val profile: JdbcProfile
   import profile.api._
-  implicit def enumColumnType[E: CaseEnum: ClassTag]: BaseColumnType[E] = {
+  implicit def enumColumnType[E: CaseEnum: ClassTag] = {
     val enum: CaseEnum[E] = CaseEnum[E]
     MappedColumnType.base[E, String](String.valueOf, (s: String) => {
       enum.fromString(s).getOrElse(throw new IllegalArgumentException(s"Unrecognizable value $s in $enum"))
