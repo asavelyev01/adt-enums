a0b2fa8e907cfdf03af670a0d6deef6c3c83ecf7
anton.savelyev <anton.savelyev@vimpelcom.com>
2018-11-14 20:59:29 +0300

    Fix slick column type derivation

--- case-enum-slick/src/main/scala/com/veon/ep/enums/slick/EnumColumnTypes.scala
+++ case-enum-slick/src/main/scala/com/veon/ep/enums/slick/EnumColumnTypes.scala
@@ -8,7 +8,7 @@ import scala.reflect.ClassTag
 trait EnumColumnTypes {
   val profile: JdbcProfile
   import profile.api._
-  implicit def enumColumnType[E: CaseEnum: ClassTag] = {
+  implicit def enumColumnType[E: CaseEnum: ClassTag]: BaseColumnType[E] = {
     val enum: CaseEnum[E] = CaseEnum[E]
     MappedColumnType.base[E, String](String.valueOf, (s: String) => {
       enum.fromString(s).getOrElse(throw new IllegalArgumentException(s"Unrecognizable value $s in $enum"))
